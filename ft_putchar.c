6b1b243acaada4cf1951ca2a254552fee2ad100b
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-06-10 22:27:39 +0200

    version inicial

---
+++ ft_putchar.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 18:07:22 by juagomez          #+#    #+#             */
+/*   Updated: 2024/06/10 18:40:00 by juagomez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+/* Toma un carácter 'c' como entrada y lo escribe en la salida estándar 
+(descriptor de archivo 1). Luego devuelve el valor 1 para indicar éxito, 
+normalmente utilizado para la impresión de caracteres en programas. */
+
+void	ft_putchar(char character)
+{
+	write(1, &character, 1);
+}
