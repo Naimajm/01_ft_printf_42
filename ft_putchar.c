b39cfb61de3b57319b56ca84cb59c635606171c2
Juanma Gomez <juanma.gomez.martin@gmail.com>
2024-06-15 22:12:25 +0200

    testeo main ft_putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: juagomez <juagomez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 18:07:22 by juagomez          #+#    #+#             */
-/*   Updated: 2024/06/11 21:46:02 by juagomez         ###   ########.fr       */
+/*   Updated: 2024/06/15 21:33:00 by juagomez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,12 @@ int ft_putchar(char character)
 	write(1, &character, 1);
     return (1);
 }
+
+/* int	main(int argn, char **argv)
+{
+	char caracter = argv[1][0];
+    int num = ft_putchar(caracter);
+	if (argn > 1)
+		printf("\n num caracteres impresos: %d \n",num);
+	return (0);
+} */
